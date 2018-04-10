ba55d0c0c6c14bc0de01a9f09955d6c0b406a740
Vijaya Malla <vijayamalla@outlook.com>
2018-04-10 13:15:28 -0400

    Sample/Values controller, Global and Route Config files.

---
+++ EmptyProjectToWebApi/Controllers/SampleController.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.Http;
+
+namespace EmptyProjectToWebApi.Controllers
+{
+    public class SampleController : ApiController
+    {
+    }
+}
---
+++ EmptyProjectToWebApi/Startup/RouteConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace EmptyProjectToWebApi
+{
+    public static class RouteConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApiRoute",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+                );
+        }
+    }
+}
